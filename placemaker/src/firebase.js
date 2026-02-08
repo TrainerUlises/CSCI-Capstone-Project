f4eef2918353eeef09295054aee412efb4d930ce
TrainerUlises <ulisesromero062602@gmail.com>
2026-02-08 14:45:13 -0500

    Set up Firebase backend config and environment variable support

---
+++ placemaker/src/firebase.js
@@ -0,0 +1,25 @@
+// importing firebase functions
+import {initializeApp} from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+import {getAuth} from "firebase/auth";
+
+const firebaseConfig = 
+{
+
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+
+};
+
+// initializing firebase application, connects to the project
+const app = initializeApp(firebaseConfig);
+
+//Firestore = our database
+export const db = getFirestore(app);
+
+// authentication (login/signup/creating account)
+export const auth = getAuth(app);
