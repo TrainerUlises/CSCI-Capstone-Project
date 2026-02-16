847c03caf4831d156d1434a108c9c6cfa551a7e7
TrainerUlises <ulisesromero062602@gmail.com>
2026-02-16 15:16:38 -0500

    Added Signup page, ZIP field, and login redirect link

--- placemaker/src/App.jsx
+++ placemaker/src/App.jsx
@@ -7,6 +7,8 @@ import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Login from './routes/Login'
 import './App.css'
+import Signup from './routes/Signup'
+
 
 
 function App() {
@@ -30,6 +32,7 @@ function App() {
       </button>
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
     </>
   )
