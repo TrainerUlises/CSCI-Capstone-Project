55f3f0dafbd2416b56314bdaeaae45d3c908dd9d
TrainerUlises <ulisesromero062602@gmail.com>
2026-02-25 21:24:16 -0500

    changes made to show that 1. user is logged in and will display that in console log, 2. user can refresh and still be stored

--- placemaker/src/main.jsx
+++ placemaker/src/main.jsx
@@ -1,12 +1,21 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext' 
 import './index.css'
 import App from './App.jsx'
-import './firebase'
+import './firebase'  // initializes Firebase
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+  <StrictMode>
+    {/* AuthProvider makes the logged-in user available everywhere */}
+    <AuthProvider>
+
+      {/* Router will be handling navigation between pages */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+
+    </AuthProvider>
+  </StrictMode>
 );
